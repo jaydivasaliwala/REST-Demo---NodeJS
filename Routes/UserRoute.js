948ae5e5aacec00722bdd14ad4cb0e8aa30364b1
Jay Divasaliwala <jaydivasaliwala2001@gmail.com>
2024-10-06 12:26:52 +0530

    Create API for Update user

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,10 +1,11 @@
 const {Router} = require('express')
-const { GetAllUser, GetUser, DeleteUser, AddUser } = require('../Controller/UserController')
+const { GetAllUser, GetUser, DeleteUser, AddUser, UpdateUser } = require('../Controller/UserController')
 const userRoute = Router()
 
 userRoute.get("/getuser",GetAllUser)
 userRoute.get("/getuser/:id",GetUser)
 userRoute.delete("/deleteuser",DeleteUser)
 userRoute.post("/adduser",AddUser)
+userRoute.put("/updateuser",UpdateUser)
 
 module.exports = {userRoute}
