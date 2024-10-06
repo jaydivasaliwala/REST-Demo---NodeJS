ad101ae7994abdb1a52893ea044766c39bd2de08
Jay Divasaliwala <jaydivasaliwala2001@gmail.com>
2024-10-06 11:22:10 +0530

    Add CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { responseModel } = require('./Model/ResponseModel');
 const { userRoute } = require('./Routes/UserRoute');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
